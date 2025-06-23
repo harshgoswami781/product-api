85d9e1bb275d3c7b4c2f2a1a0e92694fa479ebb1
harshgoswami781 <115588024+harshgoswami781@users.noreply.github.com>
2025-06-23 21:31:26 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dataset = "flutter-php";
+$servername = "sql101.infinityfree.com";
+$username = "if0_39263982";
+$password = "YR7Nv1t3RkgL6HN";
+$dataset = "if0_39263982_sewa_app";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dataset", $username, $password);
